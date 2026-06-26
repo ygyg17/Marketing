a6ad9ce777b5096e3085373c57449f2609ae860b
ygyg17 <yogisuryawan0000@gmail.com>
2026-06-26 16:04:53 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  // GANTI URL DI BAWAH DENGAN WEB APP URL DARI GOOGLE APPS SCRIPT ANDA
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwJWrSQiv6gPa2Gax9rzXRBZn57uxjUTqjf1UvFNa-7wzGmlwfCJuZ8cuDtBk3X68navQ/exec" 
+};
