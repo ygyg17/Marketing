5acdff121f9f1fc7018072de55022cdc37fc8f0c
ygyg17 <yogisuryawan0000@gmail.com>
2026-07-02 13:53:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 const CONFIG = {
+  // GANTI URL DI BAWAH DENGAN WEB APP URL DARI GOOGLE APPS SCRIPT ANDA (existing project)
   GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwJWrSQiv6gPa2Gax9rzXRBZn57uxjUTqjf1UvFNa-7wzGmlwfCJuZ8cuDtBk3X68navQ/exec",
 
-  ADMIN_GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbz_AlZTjEjfpabEsRxLB_DDGOH6hUQMBYegGv1aakS_Zz4qlqICqe4nHdfWugJ5HQLWEw/exec"
+  // Web App URL untuk Administration List (Code.gs baru, connect ke spreadsheet administration)
+  ADMIN_GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbz_AlZTjEjfpabEsRxLB_DDGOH6hUQMBYegGv1aakS_Zz4qlqICqe4nHdfWugJ5HQLWEw/exec",
+
+  // Web App URL untuk Auth (Auth-Code.gs, project GAS terpisah khusus login)
+  AUTH_GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbz5dNAdr0ZoDqQdNGEYRd7BizQEE1H3OvkkAYBE3-60FnEDfz3ysCjRc0WIJlBitW7m/exec"
 };
