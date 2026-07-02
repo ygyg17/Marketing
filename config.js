548fa3c768af83db7ee33b095c9f5cc961c99963
ygyg17 <yogisuryawan0000@gmail.com>
2026-07-02 13:29:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
-  // GANTI URL DI BAWAH DENGAN WEB APP URL DARI GOOGLE APPS SCRIPT ANDA
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwJWrSQiv6gPa2Gax9rzXRBZn57uxjUTqjf1UvFNa-7wzGmlwfCJuZ8cuDtBk3X68navQ/exec" 
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwJWrSQiv6gPa2Gax9rzXRBZn57uxjUTqjf1UvFNa-7wzGmlwfCJuZ8cuDtBk3X68navQ/exec",
+
+  ADMIN_GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbz_AlZTjEjfpabEsRxLB_DDGOH6hUQMBYegGv1aakS_Zz4qlqICqe4nHdfWugJ5HQLWEw/exec"
 };
